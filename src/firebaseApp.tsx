5ee0f9e805bdb35bda88f71037af1135fe6651ef
tofuchic <tofuchic@protonmail.com>
2022-08-12 17:30:25 +0900

    GitHub認証を追加し、GitHubのAPIをコールする処理を実装

---
+++ src/firebaseApp.tsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAT3H-OhFCjO_3Td5i2fdO5L3uf46q1a7E",
+  authDomain: "scoooool-89933.firebaseapp.com",
+  projectId: "scoooool-89933",
+  storageBucket: "scoooool-89933.appspot.com",
+  messagingSenderId: "311528503017",
+  appId: "1:311528503017:web:a34057451b2a1178eda278",
+};
+
+initializeApp(firebaseConfig);
