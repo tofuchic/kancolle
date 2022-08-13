e9e9e9b080ebdfd59e5eb97056e6d4dc4cf9e4a8
tofuchic <tofuchic@protonmail.com>
2022-08-14 01:54:49 +0900

    Reactのgithub pages対応 (#6)

--- src/router/pages/App.tsx
+++ src/router/pages/App.tsx
@@ -1,22 +1,32 @@
-import { BrowserRouter, useRoutes } from "react-router-dom";
+import { HashRouter, Link, useRoutes } from "react-router-dom";
 import { Home } from "router/pages/Home";
 import { AuthProvider } from "services/context/AuthProvider";
 import Login from "./Login";
 
 const RootRoutes = () => {
   return useRoutes([
-    { path: "/scoooool", element: <Home /> },
-    { path: "/scoooool/login", element: <Login /> },
+    { path: "/home", element: <Home /> },
+    { path: "/login", element: <Login /> },
   ]);
 };
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
       <AuthProvider>
         <RootRoutes />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
