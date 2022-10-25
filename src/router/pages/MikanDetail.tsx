e0f4b285ffbdd39ad3ee66a30f129c580e74b93d
tofuchic <tofuchic@protonmail.com>
2022-10-26 00:38:47 +0900

    バーを表示して、firestoreに投げる値を動的に変化させる

--- src/router/pages/MikanDetail.tsx
+++ src/router/pages/MikanDetail.tsx
@@ -4,6 +4,9 @@ import { doc, getDoc } from 'firebase/firestore'
 import logo from '@/assets/logo.svg'
 import { auth, db } from '@/services/auth/firebase'
 import TextField from '@mui/material/TextField'
+import Slider from '@mui/material/Slider'
+import Box from '@mui/material/Box'
+import styled from '@emotion/styled'
 
 interface Mikan {
   id: string
@@ -22,6 +25,42 @@ export const MikanDetail = (): JSX.Element => {
     note: 'memoです'
   })
   const [mikanNote, setMikanNote] = useState<string>()
+
+  function mikanTaste(value: number): string {
+    return `${value}`
+  }
+
+  function mikanTasteChangeCommitted(
+    _event: React.SyntheticEvent | Event,
+    value: number | number[]
+  ): void {
+    setMikan({
+      id: mikan.id,
+      userId: mikan.userId,
+      taste: Number(value),
+      texture: mikan.texture,
+      note: mikan.note
+    })
+    // fire patch to db
+  }
+
+  function mikanTexture(value: number): string {
+    return `${value}`
+  }
+
+  function mikanTextureChangeCommitted(
+    _event: React.SyntheticEvent | Event,
+    value: number | number[]
+  ): void {
+    setMikan({
+      id: mikan.id,
+      userId: mikan.userId,
+      taste: mikan.taste,
+      texture: Number(value),
+      note: mikan.note
+    })
+    // fire patch to db
+  }
 
   useEffect(() => {
     void getMyMikanData()
@@ -94,6 +133,40 @@ export const MikanDetail = (): JSX.Element => {
       <div>
         <h3>{mikan.userId}さんの評価</h3>
       </div>
+      <TateWrapper>
+        <Wrapper>
+          <Column>酸っぱい</Column>
+          <Box sx={{ width: 300 }}>
+            <Slider
+              aria-label="Taste"
+              defaultValue={mikan.taste}
+              getAriaValueText={mikanTaste}
+              onChangeCommitted={mikanTasteChangeCommitted}
+              step={1}
+              marks
+              min={-5}
+              max={5}
+            />
+          </Box>
+          <Column>甘い</Column>
+        </Wrapper>
+        <Wrapper>
+          <Column>しゃきしゃき</Column>
+          <Box sx={{ width: 300 }}>
+            <Slider
+              aria-label="Texture"
+              defaultValue={mikan.texture}
+              getAriaValueText={mikanTexture}
+              onChangeCommitted={mikanTextureChangeCommitted}
+              step={1}
+              marks
+              min={-5}
+              max={5}
+            />
+          </Box>
+          <Column>とろとろ</Column>
+        </Wrapper>
+      </TateWrapper>
       <div>
         <a>酸っぱさ</a>
         {mikan.taste}
@@ -115,3 +188,20 @@ export const MikanDetail = (): JSX.Element => {
     </div>
   )
 }
+
+const TateWrapper = styled.section`
+  align-items: center;
+`
+
+const Wrapper = styled.section`
+  display: flex;
+  align-items: center;
+`
+
+const Column = styled.span`
+  margin-left: 24px;
+  margin-right: 24px;
+  width: 100px;
+  display: block;
+  text-align: center;
+`
