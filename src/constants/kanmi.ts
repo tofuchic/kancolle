e06b8d56eb66caa99e55a1b8148583886b5ceed4
tofuchic <tofuchic@protonmail.com>
2022-11-11 01:23:46 +0900

    かんみトップページをイメージして作成

---
+++ src/constants/kanmi.ts
@@ -0,0 +1,21 @@
+export interface MonthlyKanmi {
+  year: number
+  month: number
+  mikanList: string[]
+}
+
+export const kanmi: MonthlyKanmi[] = [
+  {
+    year: 2022,
+    month: 11,
+    mikanList: [
+      'tagutiwase',
+      'kimurawase',
+      'orogurosu',
+      'yamashitabeniwase',
+      'kanjukuyurawase',
+      'amakusa',
+      'benimadonnna',
+    ],
+  },
+]
--- src/router/pages/Home.tsx
+++ src/router/pages/Home.tsx
@@ -1,43 +1,45 @@
-import { useState } from 'react'
-import logo from '@/assets/logo.svg'
-import '@/styles/App.css'
 import '@/services/auth/firebase'
+import styled from '@emotion/styled'
+import { kanmi } from '@/constants/kanmi'
+import { Grid } from '@mui/material'
+
+const KanmiList = (): React.ReactElement => {
+  const kanmiList = kanmi.map((monthlyKanmi, index) => {
+    const mikanList = monthlyKanmi.mikanList.map((mikan, index) => {
+      return (
+        <div key={index}>
+          <img
+            className="image-mikan"
+            // TODO: 清原から画像使う許可を貰うか、届いた後に自分で写真を撮り、publicディレクトリ下に配置する
+            // src={`https://github.com/tofuchic/kancolle/raw/main/public/mikan/${mikan}.png`}
+            src={`https://github.com/tofuchic/kancolle/raw/main/public/mikan/fruit_orange.png`}
+          />
+        </div>
+      )
+    })
+    return (
+      <Grid item xs={12} md={6} key={index}>
+        <h2>
+          {monthlyKanmi.year}年{monthlyKanmi.month}月配送
+        </h2>
+        <YokoWrapper>{mikanList}</YokoWrapper>
+      </Grid>
+    )
+  })
+  return <>{kanmiList}</>
+}
 
 export const Home = (): React.ReactElement => {
-  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>柑橘サブスクリプション「かんみ」のみかん</h1>
+      <KanmiList></KanmiList>
     </div>
   )
 }
+
+const YokoWrapper = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: left;
+`
