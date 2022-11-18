e3350a1ee6f8460630b6096278665b38a4973445
YuichiSemura <semura.yuichi@gmail.com>
2022-11-18 20:35:01 +0900

    SwipeableDrawerに変更

--- src/router/pages/App.tsx
+++ src/router/pages/App.tsx
@@ -3,7 +3,7 @@ import { styled, useTheme, createTheme } from '@mui/material/styles'
 import { HashRouter, useRoutes } from 'react-router-dom'
 import { AuthProvider } from '@/services/context/AuthProvider'
 import Box from '@mui/material/Box'
-import Drawer from '@mui/material/Drawer'
+import SwipeableDrawer from '@mui/material/SwipeableDrawer'
 import CssBaseline from '@mui/material/CssBaseline'
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
@@ -58,45 +58,6 @@ const themeColor = createTheme({
   },
 })
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}))
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}))
-
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
@@ -107,8 +68,8 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }))
 
 const links = [
-  { link: '#/home', text: 'Home', icon: <HomeIcon/> },
-  { link: '#/login', text: 'Login', icon: <LoginIcon/> },
+  { link: '#/home', text: 'Home', icon: <HomeIcon /> },
+  { link: '#/login', text: 'Login', icon: <LoginIcon /> },
   {
     link: '#/mikan?displayName=cut_fruit_orange,fruit_ao_mikan',
     text: 'n月のみかん',
@@ -120,7 +81,7 @@ const links = [
 export const App: React.FunctionComponent = () => {
   const theme = useTheme()
   const [open, setOpen] = useState<boolean>(false)
-  
+
   function handleDrawer(): void {
     setOpen(!open)
   }
@@ -129,7 +90,7 @@ export const App: React.FunctionComponent = () => {
     <HashRouter basename="/">
       <ThemeProvider theme={themeColor}>
         <Box sx={{ display: 'flex' }}>
-          <AppBar position="fixed" open={open} color="secondary">
+          <MuiAppBar position="fixed" color="secondary">
             <Toolbar>
               <IconButton
                 color="inherit"
@@ -144,8 +105,8 @@ export const App: React.FunctionComponent = () => {
                 柑これ
               </Typography>
             </Toolbar>
-          </AppBar>
-          <Drawer
+          </MuiAppBar>
+          <SwipeableDrawer
             sx={{
               width: drawerWidth,
               flexShrink: 0,
@@ -155,16 +116,16 @@ export const App: React.FunctionComponent = () => {
                 backgroundColor: '#ef7c1d',
               },
             }}
-            variant="persistent"
-            anchor="left"
             open={open}
+            onClose={handleDrawer}
+            onOpen={handleDrawer}
           >
             <DrawerHeader>
               <IconButton onClick={handleDrawer}>
                 {theme.direction === 'ltr' ? (
-                  <ChevronLeftIcon sx={{color: "#f0f0f0"}}/>
+                  <ChevronLeftIcon sx={{ color: '#f0f0f0' }} />
                 ) : (
-                  <ChevronRightIcon sx={{color: "#f0f0f0"}}/>
+                  <ChevronRightIcon sx={{ color: '#f0f0f0' }} />
                 )}
               </IconButton>
             </DrawerHeader>
@@ -172,7 +133,9 @@ export const App: React.FunctionComponent = () => {
               {links.map((text, index) => (
                 <ListItem key={index} disablePadding>
                   <ListItemButton component="a" href={text.link}>
-                    <ListItemIcon sx={{color: "#f0f0f0", ml: "6px"}}>{text.icon}</ListItemIcon>
+                    <ListItemIcon sx={{ color: '#f0f0f0', ml: '6px' }}>
+                      {text.icon}
+                    </ListItemIcon>
                     <ListItemText
                       primary={text.text}
                       primaryTypographyProps={{
@@ -185,13 +148,13 @@ export const App: React.FunctionComponent = () => {
                 </ListItem>
               ))}
             </List>
-          </Drawer>
-          <Main open={open}>
+          </SwipeableDrawer>
+          <Box sx={{width: '100%'}}>
             <DrawerHeader />
             <AuthProvider>
               <RootRoutes />
             </AuthProvider>
-          </Main>
+          </Box>
         </Box>
       </ThemeProvider>
     </HashRouter>
