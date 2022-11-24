005c6e7526b433b124738e1eff9c87681da4ffcf
tofuchic <tofuchic@protonmail.com>
2022-11-25 00:43:16 +0900

    Refactor/replace all component with mui (#36)
    * 大きすぎるフォントを小さく
    
    * タイトルフォントの大きさを統一
    
    * 文章をmuiで置換
    
    * marginとかpaddingをmuiのsxで指定
    
    * pタグをTypographyに置換
    
    * divをBoxに置換

--- src/router/pages/Login.tsx
+++ src/router/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useAuth } from '@/services/context/AuthProvider'
 import '@/styles/App.css'
 import TwitterIcon from '@mui/icons-material/Twitter'
-import { Button, Box, Container } from '@mui/material'
+import { Button, Box, Container, Typography } from '@mui/material'
 
 export const Login = (): React.ReactElement => {
   const { currentUser, login, logout } = useAuth()
@@ -9,11 +9,11 @@ export const Login = (): React.ReactElement => {
   return (
     <div className="App">
       <Container>
-        <Box sx={{ padding: '16px' }}>
+        <Box sx={{ m: 3 }}>
           {currentUser == null && (
             <Button variant="contained" onClick={login}>
               <TwitterIcon style={{ color: '#1DA1F2' }} />
-              Twitterでログイン
+              <Typography variant={'body1'}>Twitterでログイン</Typography>
             </Button>
           )}
           {currentUser != null && (
@@ -21,9 +21,11 @@ export const Login = (): React.ReactElement => {
               {currentUser.photoURL != null && (
                 <img className="icon" src={currentUser.photoURL} />
               )}
-              <p>{currentUser.displayName}としてログイン中</p>
+              <Typography sx={{ my: 3 }} variant={'body1'}>
+                {currentUser.displayName}としてログイン中
+              </Typography>
               <Button variant="contained" onClick={logout}>
-                ログアウト
+                <Typography variant={'body1'}>ログアウト</Typography>
               </Button>
             </div>
           )}
