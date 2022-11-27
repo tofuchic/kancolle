759b6fb9264f8f6c98480d227c3538ebd6902440
tofuchic <tofuchic@protonmail.com>
2022-11-27 17:54:29 +0900

    Refactor/links (#40)
    * HomeをKanmiにrenameし、Homeを別途作成
    
    * 画面遷移を修正
    
    * ホーム画面に表示するメッセージを変更
    
    * コピペミスを修正

--- src/router/pages/Home.tsx
+++ src/router/pages/Home.tsx
@@ -1,56 +1,11 @@
-import '@/services/auth/firebase'
-import { kanmi } from '@/constants/kanmi'
-import { Box, Grid, Typography } from '@mui/material'
-import { Link as MuiLink } from '@mui/material'
-import OpenInNewIcon from '@mui/icons-material/OpenInNew'
-import { Link } from 'react-router-dom'
-
-const KanmiList = (): React.ReactElement => {
-  const kanmiList = kanmi.map((monthlyKanmi, index) => {
-    return (
-      <Grid item xs={12} md={10} lg={8} xl={6} key={index}>
-        <Grid container sx={{ p: 1 }}>
-          <Typography sx={{ my: 3 }} variant={'h2'} fontSize="1.5em">
-            <Link to={'/mikan?displayName=' + monthlyKanmi.mikanList}>
-              {monthlyKanmi.year}年{monthlyKanmi.month}月配送
-            </Link>
-          </Typography>{' '}
-        </Grid>
-        <Grid container>
-          <Grid item xs={12} key={index}>
-            <img
-              className="image-mikan"
-              src={monthlyKanmi.imgURL}
-              style={{
-                width: '100%',
-                height: 'auto',
-              }}
-            />
-          </Grid>
-        </Grid>
-      </Grid>
-    )
-  })
-  return <>{kanmiList}</>
-}
+import { Alert, Box } from '@mui/material'
 
 export const Home = (): React.ReactElement => {
   return (
     <Box sx={{ p: 2 }}>
-      <Typography sx={{ my: 3 }} variant={'h1'} fontSize="2em">
-        <MuiLink
-          href={'https://shop.kanmi.co.jp/'}
-          underline="hover"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          柑橘サブスクリプション「かんみ」<OpenInNewIcon></OpenInNewIcon>
-        </MuiLink>
-        のみかん
-      </Typography>{' '}
-      <Grid container justifyContent={'center'}>
-        <KanmiList></KanmiList>
-      </Grid>
+      <Alert sx={{ my: 3 }} severity="info">
+        非公式サイトです。
+      </Alert>
     </Box>
   )
 }
